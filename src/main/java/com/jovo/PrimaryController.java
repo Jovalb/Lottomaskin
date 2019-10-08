313dfbf8d98efa8425a044dbf8d6e4efa68c6469
Jovalb <jovorapper1@gmail.com>
2019-10-08 17:20:42 +0200

    Fått laget programmet men sliter med å lage en executable fil som kan kjøres på pcen

--- src/main/java/com/jovo/PrimaryController.java
+++ src/main/java/com/jovo/PrimaryController.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -26,13 +27,71 @@ public class PrimaryController {
         App.setRoot("secondary");
     }
 
+    public static int gcd(int a, int b)  // Euklids algoritme
+    {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public static void rotasjon(int [] a, int k){
+        int n = a.length;
+        if (n < 2) return;         // ingen rotasjon
+        if ((k %= n) < 0) k += n;                     // motsatt vei?
+
+        int s = gcd(n, k);                            // største felles divisor
+
+        for (int e = 0; e < s; e++)                   // antall sykler
+        {
+            int verdi = a[e];                          // hjelpevariabel
+
+            for (int i = e - k, j = e; i != e; i -= k)  // løkke
+            {
+                if (i < 0) i += n;                        // sjekker fortegnet til i
+                a[j] = a[i];
+                j = i;                       // kopierer og oppdaterer j
+            }
+
+            a[e + k] = verdi;                           // legger tilbake verdien
+        }
+    }
+
+    public static String arrayToString(int [] a){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('[');
+
+        for (int i = 0; i < a.length-1; i++) {
+            stringBuilder.append(a[i]).append(", ");
+        }
+        stringBuilder.setLength(stringBuilder.length() - 2);
+        stringBuilder.append(']');
+
+        return stringBuilder.toString();
+    }
+
     @FXML
     private void test (){
-        String inputListe = inputBox.getText();
-        char[] listeArray = inputListe.toCharArray();
-        System.out.println(inputListe.toCharArray());
-        observableList.add(Arrays.toString(listeArray));
-        listeView.setItems(observableList);
+        String [] input = inputBox.getText().split("-");
+        if (input.length < 8){
+            Alert error = new Alert(Alert.AlertType.ERROR,"Du har skrevet inn for få tall!");
+            error.showAndWait();
+        } else if (input.length > 8){
+            Alert error = new Alert(Alert.AlertType.ERROR,"Du har skrevet inn for få tall!");
+            error.showAndWait();
+        } else {
+            int [] listeArray = new int [input.length];
+
+            for (int i = 0; i < input.length; i++) {
+                listeArray[i] = Integer.parseInt(input[i]);
+            }
+            observableList.add(arrayToString(listeArray));
+            //maks 8 rekker for 8 tall
+            for (int i = 0; i < listeArray.length-1; i++) {
+                rotasjon(listeArray,1);
+                System.out.println(arrayToString(listeArray));
+                observableList.add(arrayToString(listeArray));
+            }
+            listeView.setItems(observableList);
+        }
+
     }
 
 
